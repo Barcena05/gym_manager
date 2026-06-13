597531e8c005c3034d7f7fb97bdeea7a835eefa9
Ernesto Bárcena Trujillo <114311116+Barcena05@users.noreply.github.com>
2026-06-13 11:25:51 -0400

    Rename 20260611_add_member_photo.sql to 20260611120000_add_member_photo.sql

---
+++ src-tauri/migrations/20260611120000_add_member_photo.sql
@@ -0,0 +1,2 @@
+-- Add photo_path column to members table
+ALTER TABLE members ADD COLUMN photo_path TEXT;
--- src-tauri/migrations/20260611_add_member_photo.sql
+++
@@ -1,2 +0,0 @@
--- Add photo_path column to members table
-ALTER TABLE members ADD COLUMN photo_path TEXT;
